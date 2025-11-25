d1abc3e8ad61cd24a4e78832f5700268f515dee6
João Slengman <jpcslengman@outlook.com>
2025-11-25 01:22:26 -0300

    Feat: Reestruturação do sistema de internacionalização (i18n)
    - Padronização da pasta de idiomas: renomeada de 'Languages' para 'lang'.
    - Atualização da lógica de carregamento em 'includes/i18n.php'.
    - Migração e adição de arquivos de tradução para:
      - Português (pt-br)
      - Inglês (en-us)
      - Espanhol (es-es)
      - Italiano (it-it)
      - Japonês (ja-jp)
      - Chinês (zh-cn)
      - Hindi (hi-in)
    - Correção de chaves de tradução (arrays PHP).

--- Includes/i18n.php
+++ Includes/i18n.php
@@ -1,32 +1,29 @@
 <?php
-if(session_status() === PHP_SESSION_NONE){
+if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
-if (isset($_GET['lang'])){
-    if ($_GET['lang'] == 'en-us'){
-        $_SESSION['idioma']= 'en-us';
+if (isset($_GET['lang'])) {
+    if ($_GET['lang'] == 'en-us') {
+        $_SESSION['idioma'] = 'en-us';
     } elseif ($_GET['lang'] == 'hi-in') {
-        $_SESSION['idioma']= 'hi-in';
+        $_SESSION['idioma'] = 'hi-in';
     } elseif ($_GET['lang'] == 'pt-br') {
-        $_SESSION['idioma']= 'pt-br';
+        $_SESSION['idioma'] = 'pt-br';
     } elseif ($_GET['lang'] == 'zh-cn') {
         $_SESSION['idioma'] = 'zh-cn';
-    } elseif($_GET['lang'] == 'es-es'){
+    } elseif ($_GET['lang'] == 'es-es') {
         $_SESSION['idioma'] = 'es-es';
-    } elseif($_GET['lang'] == 'ja-jp'){
-        $_SESSION['idioma'] ='ja-jp';
-    } elseif($_GET['lang'] == 'it-it'){
+    } elseif ($_GET['lang'] == 'ja-jp') {
+        $_SESSION['idioma'] = 'ja-jp';
+    } elseif ($_GET['lang'] == 'it-it') {
         $_SESSION['idioma'] = 'it-it';
-    }
-    else{
-        $_SESSION ['idioma'] = 'pt-br'; 
-    }
-} 
-
-if   (!isset($_SESSION['idioma'])){
+    } else {
         $_SESSION['idioma'] = 'pt-br';
+    }
 }
 
-$lang = require_once(__DIR__ . '/../Languages/' . $_SESSION['idioma'] . '.php');
+if (!isset($_SESSION['idioma'])) {
+    $_SESSION['idioma'] = 'pt-br';
+}
 
-?>
+$lang = require_once(__DIR__ . '/../lang/' . $_SESSION['idioma'] . '.php');
